0f45ef9f322f41b407a4824d8318c2e309ad726f
rubydusa <rubydusa@protonmail.com>
2022-11-28 11:09:09 +0200

    implement field element and mod arithmetic error

--- src/ff/mod.rs
+++ src/ff/mod.rs
@@ -1,13 +1,21 @@
-use std::num::Wrapping;
 use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Clone, Copy)]
-struct FieldElement<const P: u32> {
+pub struct FieldElement<const P: u32> {
     val: u32,
+}
+
+#[derive(Debug)]
+enum ModularArithmeticError {
+    ZeroModulo,
+    ZeroMultiplicativeInverse,
 }
 
 impl<const P: u32> FieldElement<P> {
     pub fn new(val: u32) -> FieldElement<P> {
+        if P == 0 {
+            panic!("FieldElement can't have 0 as a modulo")
+        }
         FieldElement { val }
     }
 
@@ -16,47 +24,116 @@ impl<const P: u32> FieldElement<P> {
     }
 }
 
-fn modulus_add(a: u32, b: u32, n: u32) -> u32 {
-    u32::try_from((u64::from(a) + u64::from(b)).rem_euclid(u64::from(n)))
-        .expect("unexpected overflow in modulus addition")
+impl<const P: u32> Add for FieldElement<P> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            val: modulus_add(self.val, rhs.val, P).expect("something went terribly wrong"),
+        }
+    }
 }
 
-fn modulus_sub(a: u32, b: u32, n: u32) -> u32 {
-    if a > b {
-        (a - b).rem_euclid(n)
+impl<const P: u32> Sub for FieldElement<P> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            val: modulus_sub(self.val, rhs.val, P).expect("something went terribly wrong"),
+        }
+    }
+}
+
+impl<const P: u32> Mul for FieldElement<P> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            val: modulus_mul(self.val, rhs.val, P).expect("something went terribly wrong"),
+        }
+    }
+}
+
+impl<const P: u32> Div for FieldElement<P> {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self {
+            val: match modulus_div(self.val, rhs.val, P) {
+                Err(ModularArithmeticError::ZeroModulo) => {
+                    panic!("something went terribly wrong")
+                }
+                Err(ModularArithmeticError::ZeroMultiplicativeInverse) => {
+                    panic!("zero multiplicative inverse")
+                }
+                Ok(new_val) => new_val,
+            },
+        }
+    }
+}
+
+fn modulus_add(a: u32, b: u32, n: u32) -> Result<u32, ModularArithmeticError> {
+    if n == 0 {
+        Err(ModularArithmeticError::ZeroModulo)
+    } else {
+        Ok(
+            u32::try_from((u64::from(a) + u64::from(b)).rem_euclid(u64::from(n)))
+                .expect("unexpected overflow in modulus addition"),
+        )
+    }
+}
+
+fn modulus_sub(a: u32, b: u32, n: u32) -> Result<u32, ModularArithmeticError> {
+    if n == 0 {
+        Err(ModularArithmeticError::ZeroModulo)
+    } else if a > b {
+        Ok((a - b).rem_euclid(n))
     } else {
         let b_inverse = additive_inverse(b, n);
-        modulus_add(a, b_inverse, n)
+        modulus_add(a, b_inverse?, n)
     }
 }
 
-fn modulus_mul(a: u32, b: u32, n: u32) -> u32 {
-    u32::try_from((u64::from(a) * u64::from(b)).rem_euclid(u64::from(n)))
-        .expect("unexpected overflow in modulus multiplication")
-}
-
-fn modulus_div(a: u32, b: u32, n: u32) -> u32 {
-    modulus_mul(a, multiplicative_inverse(b, n), n)
-}
-
-fn additive_inverse(a: u32, n: u32) -> u32 {
-    u32::try_from((-i64::from(a)).rem_euclid(i64::from(n)))
-        .expect("unexpected overflow in modulus additive invesrse")
-}
-
-// assumes gcd(a, n) == 1
-fn multiplicative_inverse(a: u32, n: u32) -> u32 {
-    if !a < n {
-        panic!("p should be greater than n");
+fn modulus_mul(a: u32, b: u32, n: u32) -> Result<u32, ModularArithmeticError> {
+    if n == 0 {
+        Err(ModularArithmeticError::ZeroModulo)
+    } else {
+        Ok(
+            u32::try_from((u64::from(a) * u64::from(b)).rem_euclid(u64::from(n)))
+                .expect("unexpected overflow in modulus multiplication"),
+        )
     }
-    if !a > 0 {
-        panic!("n and p should be positive");
+}
+
+fn modulus_div(a: u32, b: u32, n: u32) -> Result<u32, ModularArithmeticError> {
+    if n == 0 {
+        Err(ModularArithmeticError::ZeroModulo)
+    } else {
+        modulus_mul(a, multiplicative_inverse(b, n)?, n)
+    }
+}
+
+fn additive_inverse(a: u32, n: u32) -> Result<u32, ModularArithmeticError> {
+    if n == 0 {
+        Err(ModularArithmeticError::ZeroModulo)
+    } else {
+        Ok(n - a.rem_euclid(n))
+    }
+}
+
+// requires gcd(a, n) == 1
+fn multiplicative_inverse(a: u32, n: u32) -> Result<u32, ModularArithmeticError> {
+    if n == 0 {
+        return Err(ModularArithmeticError::ZeroModulo);
+    }
+    if a == 0 {
+        return Err(ModularArithmeticError::ZeroMultiplicativeInverse);
     }
     // no need to check if p is positive because if p is not positive and n is greater than equal
     // than p, n is also not positive and panic would be triggered
 
     let mut cur_n = i64::from(n);
-    let mut cur_a = i64::from(a);
+    let mut cur_a = i64::from(a.rem_euclid(n));
 
     let mut val = 1;
     let mut pre = 0;
@@ -76,6 +153,6 @@ fn multiplicative_inverse(a: u32, n: u32) -> u32 {
         }
     }
 
-    u32::try_from(val.rem_euclid(i64::from(n)))
-        .expect("multiplicative inverse modulos returned negative value")
+    Ok(u32::try_from(val.rem_euclid(i64::from(n)))
+        .expect("multiplicative inverse modulos returned negative value"))
 }
